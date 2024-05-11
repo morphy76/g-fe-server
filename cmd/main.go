dedfe6749e7bbf2cfe750c7ba1e0731633761786
Riccardo Pasquini <r.pasquini@almawave.it>
2024-05-12 00:26:21 +0200

    Build FE using builder image
    Signed-off-by: Riccardo Pasquini <r.pasquini@almawave.it>

--- cmd/main.go
+++ cmd/main.go
@@ -15,19 +15,20 @@ import (
 
 func main() {
 
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: main <context root> <static path>")
-		os.Exit(1)
+	ctxRoot, found := os.LookupEnv("CONTEXT_ROOT")
+	if !found && len(os.Args) > 1 {
+		ctxRoot = os.Args[1]
 	}
-
-	ctxRoot := os.Args[1]
-	if strings.Contains(ctxRoot, " ") || !strings.HasPrefix(ctxRoot, "/") {
+	if len(ctxRoot) == 0 || strings.Contains(ctxRoot, " ") || !strings.HasPrefix(ctxRoot, "/") {
 		fmt.Println("Invalid context root")
 		os.Exit(1)
 	}
 
-	staticPath := os.Args[2]
-	if strings.Contains(staticPath, " ") {
+	staticPath, found := os.LookupEnv("STATIC_PATH")
+	if !found && len(os.Args) > 2 {
+		staticPath = os.Args[2]
+	}
+	if len(staticPath) == 0 || strings.Contains(staticPath, " ") {
 		fmt.Println("Invalid static path")
 		os.Exit(1)
 	}
