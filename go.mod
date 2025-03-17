0bb014cc135bb0c6cbaddce57ad1c78c7d3fccc6
Riccardo Pasquini <R.Pasquini@almawave.it>
2025-03-17 16:22:30 +0100

    Update dependencies and refactor MongoDB monitoring: upgrade OIDC and OpenTelemetry packages, simplify command monitoring functions, and remove legacy code.

--- go.mod
+++ go.mod
@@ -11,19 +11,17 @@ require (
 	github.com/rs/zerolog v1.33.0
 	github.com/shirou/gopsutil/v3 v3.24.5
 	github.com/stretchr/testify v1.10.0
-	github.com/zitadel/oidc/v3 v3.35.0
-	go.mongodb.org/mongo-driver v1.17.2
+	github.com/zitadel/oidc/v3 v3.36.1
 	go.mongodb.org/mongo-driver/v2 v2.1.0
-	go.opentelemetry.io/contrib/instrumentation/github.com/gorilla/mux/otelmux v0.59.0
-	go.opentelemetry.io/contrib/instrumentation/go.mongodb.org/mongo-driver/mongo/otelmongo v0.59.0
-	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.59.0
-	go.opentelemetry.io/otel v1.34.0
-	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc v1.34.0
-	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.34.0
-	go.opentelemetry.io/otel/metric v1.34.0
-	go.opentelemetry.io/otel/sdk v1.34.0
-	go.opentelemetry.io/otel/sdk/metric v1.34.0
-	go.opentelemetry.io/otel/trace v1.34.0
+	go.opentelemetry.io/contrib/instrumentation/github.com/gorilla/mux/otelmux v0.60.0
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.60.0
+	go.opentelemetry.io/otel v1.35.0
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc v1.35.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.35.0
+	go.opentelemetry.io/otel/metric v1.35.0
+	go.opentelemetry.io/otel/sdk v1.35.0
+	go.opentelemetry.io/otel/sdk/metric v1.35.0
+	go.opentelemetry.io/otel/trace v1.35.0
 )
 
 require (
@@ -35,10 +33,10 @@ require (
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
-	github.com/golang/snappy v0.0.4 // indirect
+	github.com/golang/snappy v1.0.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
-	github.com/lufia/plan9stats v0.0.0-20250303091104-876f3ea5145d // indirect
+	github.com/lufia/plan9stats v0.0.0-20250317134145-8bc96cf8fc35 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/muhlemmer/gu v0.3.1 // indirect
@@ -47,8 +45,8 @@ require (
 	github.com/shoenig/go-m1cpu v0.1.6 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
-	github.com/tklauser/go-sysconf v0.3.14 // indirect
-	github.com/tklauser/numcpus v0.9.0 // indirect
+	github.com/tklauser/go-sysconf v0.3.15 // indirect
+	github.com/tklauser/numcpus v0.10.0 // indirect
 	github.com/twmb/murmur3 v1.1.8 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
@@ -58,16 +56,16 @@ require (
 	github.com/zitadel/logging v0.6.1 // indirect
 	github.com/zitadel/schema v1.3.0 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
-	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.34.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.35.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.5.0 // indirect
-	golang.org/x/crypto v0.35.0 // indirect
-	golang.org/x/net v0.36.0 // indirect
-	golang.org/x/oauth2 v0.27.0 // indirect
-	golang.org/x/sync v0.11.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
-	golang.org/x/text v0.22.0 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20250303144028-a0af3efb3deb // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250303144028-a0af3efb3deb // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/net v0.37.0 // indirect
+	golang.org/x/oauth2 v0.28.0 // indirect
+	golang.org/x/sync v0.12.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20250313205543-e70fdf4c4cb4 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250313205543-e70fdf4c4cb4 // indirect
 	google.golang.org/grpc v1.71.0 // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- internal/db/monitor.go
+++ internal/db/monitor.go
@@ -2,39 +2,30 @@ package db
 
 import (
 	"context"
-	"errors"
 	"fmt"
 
-	v2_bson "go.mongodb.org/mongo-driver/v2/bson"
-
-	old_event "go.mongodb.org/mongo-driver/event"
-	v2_event "go.mongodb.org/mongo-driver/v2/event"
-	"go.opentelemetry.io/contrib/instrumentation/go.mongodb.org/mongo-driver/mongo/otelmongo"
+	"go.mongodb.org/mongo-driver/v2/event"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/trace"
 )
 
-type oldStartedSignarture func(ctx context.Context, oldEvent *old_event.CommandStartedEvent)
-type newStartedSignarture func(ctx context.Context, newEvent *v2_event.CommandStartedEvent)
-type oldSucceededSignarture func(ctx context.Context, oldEvent *old_event.CommandSucceededEvent)
-type newSucceededSignarture func(ctx context.Context, newEvent *v2_event.CommandSucceededEvent)
-type oldFailedSignarture func(ctx context.Context, oldEvent *old_event.CommandFailedEvent)
-type newFailedSignarture func(ctx context.Context, newEvent *v2_event.CommandFailedEvent)
+type startedSignature func(context.Context, *event.CommandStartedEvent)
+type succeededSignature func(context.Context, *event.CommandSucceededEvent)
+type failedSignature func(context.Context, *event.CommandFailedEvent)
 
-func NewMonitor() *v2_event.CommandMonitor {
-	oldMonitor := otelmongo.NewMonitor(
-		otelmongo.WithTracerProvider(otel.GetTracerProvider()),
-	)
-	return &v2_event.CommandMonitor{
-		Started:   newCommandStartedEvent(oldMonitor.Started),
-		Succeeded: newCommandSucceededEvent(oldMonitor.Succeeded),
-		Failed:    newCommandFailedEvent(oldMonitor.Failed),
+// TODO
+func NewMonitor() *event.CommandMonitor {
+	return &event.CommandMonitor{
+		Started:   newCommandStartedEvent(),
+		Succeeded: newCommandSucceededEvent(),
+		Failed:    newCommandFailedEvent(),
 	}
 }
 
-func NewPoolMonitor() *v2_event.PoolMonitor {
-	return &v2_event.PoolMonitor{
-		Event: func(event *v2_event.PoolEvent) {
+func NewPoolMonitor() *event.PoolMonitor {
+	return &event.PoolMonitor{
+		Event: func(event *event.PoolEvent) {
 			meter := otel.GetMeterProvider().Meter("mongo")
 			useContext := context.Background()
 
@@ -67,81 +58,26 @@ func NewPoolMonitor() *v2_event.PoolMonitor {
 	}
 }
 
-func newCommandStartedEvent(old oldStartedSignarture) newStartedSignarture {
-	return func(ctx context.Context, newEvent *v2_event.CommandStartedEvent) {
-		oldCmdStartedEvent := new(old_event.CommandStartedEvent)
-		old(ctx, oldCmdStartedEvent)
-		newEvent.Command = v2_bson.Raw(oldCmdStartedEvent.Command)
-		newEvent.DatabaseName = oldCmdStartedEvent.DatabaseName
-		newEvent.RequestID = oldCmdStartedEvent.RequestID
-		newEvent.ConnectionID = oldCmdStartedEvent.ConnectionID
-		newEvent.CommandName = oldCmdStartedEvent.CommandName
-		newEvent.ServerConnectionID = oldCmdStartedEvent.ServerConnectionID64
-		if oldCmdStartedEvent.ServiceID != nil {
-			useServiceID, _ := v2_bson.ObjectIDFromHex(oldCmdStartedEvent.ServiceID.Hex())
-			newEvent.ServiceID = &useServiceID
-		}
-
+func newCommandStartedEvent() startedSignature {
+	return func(ctx context.Context, newEvent *event.CommandStartedEvent) {
 		fmt.Printf("sssssssssssstaaaaaart Event: %+v\n", newEvent)
+		trace.SpanFromContext(ctx).TracerProvider().Tracer("mongo").Start(ctx, newEvent.CommandName)
 	}
 }
 
-func newCommandSucceededEvent(old oldSucceededSignarture) newSucceededSignarture {
-	return func(ctx context.Context, newEvent *v2_event.CommandSucceededEvent) {
-		oldCmdSucceededEvent := new(old_event.CommandSucceededEvent)
-		old(ctx, oldCmdSucceededEvent)
-		newEvent.Reply = v2_bson.Raw(oldCmdSucceededEvent.Reply)
-		newEvent.CommandName = oldCmdSucceededEvent.CommandName
-		newEvent.RequestID = oldCmdSucceededEvent.RequestID
-		newEvent.ConnectionID = oldCmdSucceededEvent.ConnectionID
-		newEvent.ServerConnectionID = oldCmdSucceededEvent.ServerConnectionID64
-		if oldCmdSucceededEvent.ServiceID != nil {
-			useServiceID, _ := v2_bson.ObjectIDFromHex(oldCmdSucceededEvent.ServiceID.Hex())
-			newEvent.ServiceID = &useServiceID
-		}
-		newEvent.DatabaseName = oldCmdSucceededEvent.DatabaseName
-		newEvent.Duration = oldCmdSucceededEvent.Duration
-		newEvent.CommandFinishedEvent = v2_event.CommandFinishedEvent{
-			CommandName:        oldCmdSucceededEvent.CommandName,
-			RequestID:          oldCmdSucceededEvent.RequestID,
-			ConnectionID:       oldCmdSucceededEvent.ConnectionID,
-			ServerConnectionID: oldCmdSucceededEvent.ServerConnectionID64,
-		}
-		if oldCmdSucceededEvent.CommandFinishedEvent.ServiceID != nil {
-			finishedServiceID, _ := v2_bson.ObjectIDFromHex(oldCmdSucceededEvent.CommandFinishedEvent.ServiceID.Hex())
-			newEvent.CommandFinishedEvent.ServiceID = &finishedServiceID
-		}
-
+func newCommandSucceededEvent() succeededSignature {
+	return func(ctx context.Context, newEvent *event.CommandSucceededEvent) {
 		fmt.Printf("sssssssuuuuucccccceeeeeesssss Event: %+v\n", newEvent)
+		_, span := trace.SpanFromContext(ctx).TracerProvider().Tracer("mongo").Start(ctx, newEvent.CommandName)
+		span.End()
 	}
 }
 
-func newCommandFailedEvent(old oldFailedSignarture) newFailedSignarture {
-	return func(ctx context.Context, newEvent *v2_event.CommandFailedEvent) {
-		oldCmdFailedEvent := new(old_event.CommandFailedEvent)
-		old(ctx, oldCmdFailedEvent)
-		newEvent.CommandName = oldCmdFailedEvent.CommandName
-		newEvent.RequestID = oldCmdFailedEvent.RequestID
-		newEvent.ConnectionID = oldCmdFailedEvent.ConnectionID
-		newEvent.ServerConnectionID = oldCmdFailedEvent.ServerConnectionID64
-		if oldCmdFailedEvent.ServiceID != nil {
-			useServiceID, _ := v2_bson.ObjectIDFromHex(oldCmdFailedEvent.ServiceID.Hex())
-			newEvent.ServiceID = &useServiceID
-		}
-		newEvent.DatabaseName = oldCmdFailedEvent.DatabaseName
-		newEvent.CommandFinishedEvent = v2_event.CommandFinishedEvent{
-			CommandName:        oldCmdFailedEvent.CommandName,
-			RequestID:          oldCmdFailedEvent.RequestID,
-			ConnectionID:       oldCmdFailedEvent.ConnectionID,
-			ServerConnectionID: oldCmdFailedEvent.ServerConnectionID64,
-		}
-		if oldCmdFailedEvent.CommandFinishedEvent.ServiceID != nil {
-			finishedServiceID, _ := v2_bson.ObjectIDFromHex(oldCmdFailedEvent.CommandFinishedEvent.ServiceID.Hex())
-			newEvent.CommandFinishedEvent.ServiceID = &finishedServiceID
-		}
-		newEvent.Failure = errors.New(oldCmdFailedEvent.Failure)
-		newEvent.Duration = oldCmdFailedEvent.Duration
-
+func newCommandFailedEvent() failedSignature {
+	return func(ctx context.Context, newEvent *event.CommandFailedEvent) {
 		fmt.Printf("ssssssfaaaaaiiiillllll Event: %+v\n", newEvent)
+		_, span := trace.SpanFromContext(ctx).TracerProvider().Tracer("mongo").Start(ctx, newEvent.CommandName)
+		span.RecordError(newEvent.Failure)
+		span.End()
 	}
 }
